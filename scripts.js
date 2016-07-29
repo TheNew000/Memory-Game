bd24bf525f0ded211b6a44dec5a0e91b3967f7f9
TheNew000 <mahkatorocks@gmail.com>
2016-07-29 15:40:32 -0400

    Working on the BG image

--- scripts.js
+++ scripts.js
@@ -1,31 +1,48 @@
-var cards = ['<img src="images/rigby.png">', '<img src="images/mordecai.png">'];
+var images = ['benson', 'death', 'eileen', 'garretbobby', 'gary', 'highfiveghost', 'margaret', 'mordecai', 'mrmaellard', 'muscleman', 'pops', 'rigby', 'sensai', 'skips', 'starla', 'thomas'];
+var rando = Math.floor(Math.random() * 32) + 1;
 
+var cardsArray = $.merge(images, images);
+shuffle(cardsArray);
 
 $(document).ready(function(){
-    var gridSize = 4;
-    var card = '<img src="images/rigby.png">';
+    var gridSize = 32;
     var mgHTML = '';
     for (var i = 0; i < gridSize; i++) {
-        if(i < 2){card=cards[0];}
-        else{card = cards[1];}
-        mgHTML += '<div class="mg-tile col-sm-3">';
+        mgHTML += '<div class="mg-tile col-sm-2">';
             mgHTML += '<div class="mg-tile-inner">';
-                mgHTML += '<div class="mg-tile-front">' + card + '</div>';
+                mgHTML += '<div class="mg-tile-front"><img src="images/' + cardsArray[i] + '.png"></div>';
                 mgHTML += '<div class="mg-tile-back"></div>';
-            mgHTML += '<div>';
-        mgHTML += '<div>';
+            mgHTML += '</div>';
+        mgHTML += '</div>';
     }
     $('.mg-contents').html(mgHTML);
     
     $('.mg-tile-inner').click(function(){
         $(this).toggleClass('flip');
         var cardsUp = $('.mg-tile-inner.flip');
+        cardsUp.parent().addClass('background-clear');
         if(cardsUp.length == 2){
-            if(cardsUp[0].src == cardsUp[1].src){
-
+            if(cardsUp.find('img')[0].src == cardsUp.find('img')[1].src){
+                cardsUp.addClass('matched');
             }else{
-                cardsUp.removeClass('flip');
+                setTimeout(function(){
+                    cardsUp.removeClass('flip');
+                    cardsUp.parent().removeClass('background-clear');
+                    console.log(cardsUp.parent());
+                }, 500);
             }
         }
     });
 });
+
+    function shuffle(array){
+        var shuff = array.length, temp, index;
+        while(shuff > 0){
+            index = Math.floor(Math.random() * shuff);
+            shuff--
+            temp = array[shuff];
+            array[shuff] = array[index];
+            array[index] = temp;
+        }
+        return array;
+    }
