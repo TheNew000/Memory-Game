e7fd84eebb132e9cf60a5a25631da44f28326fa6
TheNew000 <mahkatorocks@gmail.com>
2016-07-29 19:35:40 -0400

    improved functionality

--- scripts.js
+++ scripts.js
@@ -5,32 +5,38 @@ var cardsArray = $.merge(images, images);
 shuffle(cardsArray);
 
 $(document).ready(function(){
-    var gridSize = 32;
     var mgHTML = '';
-    for (var i = 0; i < gridSize; i++) {
-        mgHTML += '<div class="mg-tile col-sm-2">';
+    var mgHTML2 = '';
+    for (var i = 0; i < 16; i++) {
+        mgHTML += '<div class="mg-tile col-sm-3">';
             mgHTML += '<div class="mg-tile-inner">';
                 mgHTML += '<div class="mg-tile-front"><img src="images/' + cardsArray[i] + '.png"></div>';
                 mgHTML += '<div class="mg-tile-back"></div>';
             mgHTML += '</div>';
         mgHTML += '</div>';
     }
-    $('.mg-contents').html(mgHTML);
+    $('.mg-contents-left').html(mgHTML);
+
+    for (var j = 16; j < 32; j++) {
+        mgHTML2 += '<div class="mg-tile col-sm-3">';
+            mgHTML2 += '<div class="mg-tile-inner">';
+                mgHTML2 += '<div class="mg-tile-front"><img src="images/' + cardsArray[j] + '.png"></div>';
+                mgHTML2 += '<div class="mg-tile-back"></div>';
+            mgHTML2 += '</div>';
+        mgHTML2 += '</div>';
+    }
+    $('.mg-contents-right').html(mgHTML2);
     
     $('.mg-tile-inner').click(function(){
         $(this).toggleClass('flip');
         var cardsUp = $('.mg-tile-inner.flip');
-        cardsUp.parent().addClass('background-clear');
-        if(cardsUp.length == 2){
-            if(cardsUp.find('img')[0].src == cardsUp.find('img')[1].src){
-                cardsUp.addClass('matched');
-            }else{
-                setTimeout(function(){
-                    cardsUp.removeClass('flip');
-                    cardsUp.parent().removeClass('background-clear');
-                    console.log(cardsUp.parent());
-                }, 500);
-            }
+        if(cardsUp.find('img')[0].src == cardsUp.find('img')[1].src){
+            cardsUp.addClass('matched');
+            cardsUp.off("click");
+        }else{
+            setTimeout(function(){
+                cardsUp.removeClass('flip');
+            }, 500);
         }
     });
 });
@@ -39,7 +45,7 @@ $(document).ready(function(){
         var shuff = array.length, temp, index;
         while(shuff > 0){
             index = Math.floor(Math.random() * shuff);
-            shuff--
+            shuff--;
             temp = array[shuff];
             array[shuff] = array[index];
             array[index] = temp;
